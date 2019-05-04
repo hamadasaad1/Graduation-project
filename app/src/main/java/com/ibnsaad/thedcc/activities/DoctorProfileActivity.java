91229a692b1769f7f5f3112df07cb3218f09331a
eslam.faisal <Eslam.faisal@yelloco.com>
2019-05-04 18:47:00 +0200

    splash

--- app/src/main/java/com/ibnsaad/thedcc/activities/DoctorProfileActivity.java
+++ app/src/main/java/com/ibnsaad/thedcc/activities/DoctorProfileActivity.java
@@ -2,6 +2,14 @@ package com.ibnsaad.thedcc.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.ibnsaad.thedcc.R;
+import com.ibnsaad.thedcc.utils.Tools;
 
 public class DoctorProfileActivity extends AppCompatActivity {
 
@@ -9,5 +17,26 @@ public class DoctorProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_profile);
+
+        initToolbar();
+        initComponent();
     }
+
+    private void initToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_menu);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void initComponent() {
+        Tools.displayImageOriginal(this, (ImageView) findViewById(R.id.image_1), R.drawable.image_8);
+        Tools.displayImageOriginal(this, (ImageView) findViewById(R.id.image_2), R.drawable.image_8);
+        Tools.displayImageOriginal(this, (ImageView) findViewById(R.id.image_3), R.drawable.image_8);
+        Tools.displayImageOriginal(this, (ImageView) findViewById(R.id.image_4), R.drawable.image_8);
+        Tools.displayImageOriginal(this, (ImageView) findViewById(R.id.image_5), R.drawable.image_8);
+    }
+
+
 }
--- app/src/main/java/com/ibnsaad/thedcc/activities/SplashedActivity.kt
+++ app/src/main/java/com/ibnsaad/thedcc/activities/SplashedActivity.kt
@@ -1,9 +1,11 @@
-package org.nixan.splashscreenexample
+package com.ibnsaad.thedcc.activities
 
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
+import com.ibnsaad.thedcc.R
 
 /**
  * Created by nixan on 12.01.2018.
@@ -14,15 +16,11 @@ private const val ACTIVITY_AUTH = 1000
 abstract class SplashedActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        if (!isAuthenticated()) {
-            startActivityForResult(Intent(this, AuthActivity::class.java), ACTIVITY_AUTH)
+        if (true) {
+            startActivityForResult(Intent(this, LoginActivity::class.java), ACTIVITY_AUTH)
         }
-        setTheme(R.style.AppTheme_Base)
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
-    }
-
-    private fun isAuthenticated(): Boolean {
-        return getUser() != null
     }
 
     private fun onAuthenticatedCallback(resultCode: Int, data: Intent?) {
